9982ffc4fb5ec94ff0bd58265497f40c8be3e42e
Cinosargo22 <cinosargo22@gmail.com>
2022-09-25 09:23:30 -0700

    third commit

--- server.js
+++ server.js
@@ -2,17 +2,12 @@ const express = require("express");
 
 const app = express();
 const PORT = 3001;
-const path = require('path');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use( express.static('public') );
 
-app.use(apiRoutes);
-app.use(htmlRoutes);
-
+app.get('/', (req, res) => {
+    res.send("hello")
+});
 
 app.listen(PORT, () => {
     console.log("Listening to http://localhost:3001")
