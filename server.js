90ff4cb27bfd76e2681536a105fee0fee3f31315
Cinosargo22 <cinosargo22@gmail.com>
2022-09-25 10:58:12 -0700

    heroku first

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
 const app = express();
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 app.use( express.static('public') );
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
+
     res.send("hello")
 });
 
