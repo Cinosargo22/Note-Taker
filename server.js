a47fd265265fe32c53c50ab91b1c538375bb9887
Cinosargo22 <cinosargo22@gmail.com>
2022-09-25 10:46:11 -0700

    sixth commit

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 
 const app = express();
-const PORT = 3001;
-
+const PORT = process.env.PORT || 3001
 app.use( express.static('public') );
 
 app.get('/', (req, res) => {
