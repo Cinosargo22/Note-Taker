17a25f4100a52f910ab6d90096e8a4cd3a637024
Cinosargo22 <cinosargo22@gmail.com>
2022-10-03 16:29:33 -0700

    oct 3 first commit

--- server.js
+++ server.js
@@ -1,60 +1,21 @@
+const PORT = process.env.PORT || 3001;
 const express = require('express');
+const app = express();
 const fs = require('fs');
 const path = require('path');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
-const app = express();
+app.use(express.urlencoded({
+    extended: true
+}));
 
-const PORT = process.env.PORT || 3001;
-
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-app.post('/api/notes', (req, res) => {
-  fs.readFile('./db/db.json', 'utf8', (err, data) => {
-    if (err) throw err;
-    var notes = JSON.parse(data);
-    let userNote = req.body;
-    userNote.id = Math.floor(Math.random() * 5000);
-    notes.push(userNote);
-  fs.writeFile('./db/db.json', JSON.stringify(notes), (err, data) => {
-      res.json(userNote);
-  });
-  }); 
-});
-
-app.delete('/api/notes/:id', (req, res) => {
-  fs.readFile('./db/db.json', 'utf8', (err, data) => {
-    if (err) throw err;
-    let notes = JSON.parse(data);
-    const newNotes = notes.filter(note => note.id !== parseInt(req.params.id));
-  
-  fs.writeFile('./db/db.json', JSON.stringify(newNotes), (err, data) => {
-    res.json({msg: 'successfully'});
-  });
-});
-});
-
-app.get('api/notes/:id', (req, res) =>{
-  res.json(notes[req.params.id]);
-});
-
-app.get('/api/notes', (req, res) => {
-  fs.readFile('./db/db.json', 'utf8', (err, data) => {
-    if (err) throw err;
-    var notes = JSON.parse(data);
-    res.json(notes);
-  });
-});
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/notes.html'))
-});
-
-app.get('*', (req, res) => {
-   res.sendFile(path.join(__dirname, '/index.html'));
-});   
 
 app.listen(PORT, () => {
-    console.log(`App listening on PORT: ${PORT}`);
+    console.log(`API server now on port ${PORT}!`);
 });
