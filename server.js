5a45915e5b4e0637d7f09df3e2e757aba1530c0a
Cinosargo22 <cinosargo22@gmail.com>
2022-10-02 22:00:36 -0700

    oct 2

--- server.js
+++ server.js
@@ -1,15 +1,85 @@
+// Dependencies
 const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const util = require("util");
 
+
+// Handling Asynchronous Processes
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
+// Setting Up Server
 const app = express();
 const PORT = process.env.PORT || 3001;
-app.use( express.static('public') );
 
-app.get("/", (req, res) => {
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-    res.send("hello")
+
+// Static Middleware
+app.use(express.static("./develop/public"));
+
+
+// API Route | "GET" request
+app.get("/api/notes", function(req, res) {
+  readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
+      notes = [].concat(JSON.parse(data))
+      res.json(notes);
+    })
+}); 
+
+
+// API Route | "POST" request
+app.post("/api/notes", function(req, res) {
+    const note = req.body;
+    readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
+      const notes = [].concat(JSON.parse(data));
+      note.id = notes.length + 1
+      notes.push(note);
+      return notes
+    }).then(function(notes) {
+      writeFileAsync("./develop/db/db.json", JSON.stringify(notes))
+      res.json(note);
+    })
 });
 
-app.listen(PORT, () => {
-    console.log("Listening to http://localhost:3001")
-});
-    
+
+// API Route | "DELETE" request
+app.delete("/api/notes/:id", function(req, res) {
+  const idToDelete = parseInt(req.params.id);
+  readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
+    const notes = [].concat(JSON.parse(data));
+    const newNotesData = []
+    for (let i = 0; i<notes.length; i++) {
+      if(idToDelete !== notes[i].id) {
+        newNotesData.push(notes[i])
+      }
+    }
+    return newNotesData
+  }).then(function(notes) {
+    writeFileAsync("./develop/db/db.json", JSON.stringify(notes))
+    res.send('saved success!!!');
+  })
+})
+
+
+// HTML Routes
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "./develop/public/notes.html"));
+  });
+
+app.get("/", function(req, res) {
+     res.sendFile(path.join(__dirname, "./develop/public/index.html"));
+  });
+
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./develop/public/index.html"));
+ });
+
+
+// Listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
