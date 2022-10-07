d7bbae528199017d3113e48013fab38f194a4d33
Cinosargo22 <cinosargo22@gmail.com>
2022-10-06 21:58:00 -0700

    oct 6 first

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const PORT = process.env.PORT || 3001;
 const express = require('express');
-const app = express();
-const fs = require('fs');
 const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
@@ -16,6 +19,6 @@ app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+app.listen(PORT, function(){
+    console.log('App listening on PORT: ' + PORT)
 });
