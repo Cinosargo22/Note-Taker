9e697bc1bcff545e7a36d8930463bbea25774ad2
Cinosargo22 <cinosargo22@gmail.com>
2022-10-07 18:45:48 -0700

    changed port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 5000;
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
