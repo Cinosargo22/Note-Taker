9d3619da028c68d6b36ea51c29e6e09ba8383151
Cinosargo22 <cinosargo22@gmail.com>
2022-10-17 20:34:48 -0700

    oct 17

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT =  3001;
+const PORT =  process.env.PORT || 3001;
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
